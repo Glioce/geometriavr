51af044cb5b88d3431c93a2fcdeca413ea35b4d1
a <e>
2020-08-25 19:58:41 -0500

    probando gui

---
+++ testvr00/Assets/Scripts/SpriteManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SpriteRenderer.sprite = sprites[0];
+        //GetComponent<SpriteRenderer>().sprite = sprites[0];
+        //GetComponent<Image>() = sprites[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
