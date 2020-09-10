39e828c82d8d489e8f69040604df41a0c48a2fe9
Glioce <0>
2020-09-10 14:31:58 -0500

    agregar barra de carga circular

--- testvr00/Assets/Scripts/ControladorUI.cs
+++ testvr00/Assets/Scripts/ControladorUI.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+// Este script está asignado al objeto "ControladorUI"
+// Scrip asignado al objeto ControladorUI
+// Guarda respuestas, asigna texto y valores de resultados
+// Se realiza navegación como menú
+
+// Secuencia
+// 1. Mensaje con objetivo del test
+// 2. 
 
 public class ControladorUI : MonoBehaviour
 {
-    // Scrip asignado al objeto ControladorUI
-    // Guarda respuestas, asigna texto y valores de resultados
-    // Se realiza navegación como menú
+    
 
     private int[] respuestas = new int[16];
+    private int numInstruccion = 0;
 
     public GameObject preguntas;
     public GameObject portada;
+    public GameObject textobjetivo;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +31,25 @@ public class ControladorUI : MonoBehaviour
 
     // Update is called once per frame
     //void Update()
+
+    // Al hacer clic revisamos aparece otro mensaje
+    public void InstruccionSiguiente ()
+    {
+        numInstruccion++;
+
+        
+        textobjetivo.GetComponent<Text>().text = @"Este instrumento se divide de la siguiente manera:
+
+Plegado mental - 5 Reactivos 1 al 5 tipo DAT 5
+
+Dibujo Técnico - 5 Reactivos 6 al 10 Vistas e Isométrico
+
+Especiales Prehispánicos - 3 Reactivos 11 al 13 ejercicios sugeridos por el autor
+
+Giro mental 3 Reactivos - 14 al 16 ejercicios tipo MRT Y PSVT
+
+Estandarizados Nacionales - 4 Reactivos 17 al 20 tipo ENLACE Y PLANEA.";
+    }
 
     // Cuando se presiona el botón Comenzar en la portada del test
     // Grupo portada se desactiva
---
+++ testvr00/Assets/Scripts/Gaze.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
+using UnityEngine.EventSystems;
+
+public class Gaze : MonoBehaviour
+{
+    public Image circulo; //objeto que muestra un sprite circular
+    public float totalTime = 0.6f; //tiempo que se debe hacer hover para activar clic
+    public GvrReticlePointer pointer;
+    bool status; //status de conteo
+    float timer; //incrementa su valor cuando se hace hover
+
+    private void Start()
+    {
+        circulo.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    // Revisar status y actualizar timer
+    void Update()
+    {
+        if(status == true)
+        {
+            timer += Time.deltaTime;
+            circulo.fillAmount = timer / totalTime;
+        }
+        
+        if (timer >= totalTime)
+        {
+            //pointer.OnPointerClickDown();
+            //GvrPointerInputModule.Pointer.TriggerDown();
+        }
+    }
+
+    public void On()
+    {
+        status = true;
+    }
+
+    public void Off()
+    {
+        status = false;
+        timer = 0;
+        circulo.fillAmount = 0;
+    }
+}
