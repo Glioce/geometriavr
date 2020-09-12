26401f5079528c0f9b02cadcf45a818d1e6164f1
Glioce <0>
2020-09-12 09:02:04 -0500

    agregar globals

---
+++ testvr00/Assets/Scripts/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    // mail
+    public static string defaultMail = "ejemplo@mail.com";
+    public static string userMail = defaultMail;
+
+    //respuestas
+    //public static int[] respuestasCorrectas = new int[16];
+    public static int[] answers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11, 12, 13, 14, 15 };
+    public static int[] userAnswers = new int[16];
+}
--- testvr00/Assets/Scripts/TecladoNativo.cs
+++ testvr00/Assets/Scripts/TecladoNativo.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TecladoNativo : MonoBehaviour
 {
     private TouchScreenKeyboard keyboard;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        GetComponentInChildren<Text>().text = Globals.defaultMail;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (keyboard != null)
+        {
+            Globals.userMail = keyboard.text;
+            GetComponentInChildren<Text>().text = Globals.userMail;
+            //GetComponentInChildren<Text>().text = keyboard.text;
+        }
     }
 
     public void AbreTeclado()
     {
-        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        keyboard = TouchScreenKeyboard.Open(Globals.userMail, TouchScreenKeyboardType.EmailAddress);
     }
 }
